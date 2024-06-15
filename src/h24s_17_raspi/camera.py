8118d002fc5777c7f283da2313941ac358d087a8
H1rono <hronok66@gmail.com>
2024-06-15 13:15:17 +0900

    Add camera contextmanager

---
+++ src/h24s_17_raspi/camera.py
@@ -0,0 +1,16 @@
+from contextlib import contextmanager
+from typing import Any, Generator
+
+from picamera2 import Picamera2
+
+
+@contextmanager
+def acquire_camera(config: dict | None = None) -> Generator[Picamera2, Any, None]:
+    picam2 = Picamera2()
+    config = config or picam2.create_still_configuration()
+    picam2.configure(config)
+    try:
+        picam2.start()
+        yield picam2
+    finally:
+        picam2.stop()
