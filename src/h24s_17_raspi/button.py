d7663f098599e9531dd150b916865cb05747e3ad
H1rono <hronok66@gmail.com>
2024-06-15 21:55:59 +0900

    Sync with threading.Event

--- src/h24s_17_raspi/button.py
+++ src/h24s_17_raspi/button.py
@@ -4,6 +4,7 @@ import logging
 import sys
 import time
 from contextlib import contextmanager
+from threading import Event
 from typing import Any, Generator
 
 import gpiod
@@ -23,9 +24,9 @@ def acquire_button(line_name: str) -> Generator[gpiod.Line, Any, None]:
         line.release()
 
 
-def watch_button(loop: asyncio.AbstractEventLoop, button: gpiod.Line, tx: Channel[int]) -> None:
+def watch_button(loop: asyncio.AbstractEventLoop, button: gpiod.Line, tx: Channel[int], terminate: Event) -> None:
     last_value = button.get_value()
-    while True:
+    while not terminate.is_set():
         value = button.get_value()
         if value != last_value:
             asyncio.run_coroutine_threadsafe(tx.put(value), loop)
@@ -38,15 +39,23 @@ async def listen_button_event(rx: Channel[int]) -> None:
         _log.info(f"button={v}")
 
 
+async def sleep_then_set(delay: float, event: Event) -> None:
+    try:
+        await asyncio.sleep(delay)
+    finally:
+        event.set()
+
+
 async def bwatch() -> None:
     loop = asyncio.get_event_loop()
     ch: Channel[int] = Channel(10)
+    term = Event()
     with acquire_button("GPIO26") as button, concurrent.futures.ThreadPoolExecutor() as pool:
         _log.debug("exec watch-button")
-        watch_fut = loop.run_in_executor(pool, watch_button, loop, button, ch)
+        watch_fut = loop.run_in_executor(pool, watch_button, loop, button, ch, term)
         _log.debug("create listen task")
         listen_task = asyncio.create_task(listen_button_event(ch))
-        sleep_task = asyncio.create_task(asyncio.sleep(30))
+        sleep_task = asyncio.create_task(sleep_then_set(30, term))
         await asyncio.wait([watch_fut, listen_task, sleep_task], return_when=asyncio.FIRST_COMPLETED)
     _log.info("button watch loop done!")
 
