f4f8f96c21785d2ad42cb50f52e79ccf2e54a95a
H1rono <hronok66@gmail.com>
2024-06-16 10:44:40 +0900

    Split camera and button

--- src/h24s_17_raspi/client.py
+++ src/h24s_17_raspi/client.py
@@ -1,15 +1,56 @@
 import asyncio
+import io
 import logging
+import time
 from threading import Event
 
 import gpiod
 from aiochannel import Channel
 from picamera2 import Picamera2
+from PIL.Image import Image
 
 from .button import acquire_button
 from .camera import acquire_camera
 
 _log = logging.getLogger(__name__)
+
+
+def serve_button(button: gpiod.Line, button_pressed: Event, terminate: Event, delay: float = 0.1) -> None:
+    last_value = button.get_value()
+    while not terminate.is_set():
+        time.sleep(delay)
+        value = button.get_value()
+        if value == last_value:
+            continue
+        last_value = value
+        _log.debug(f"button {value=}")
+        if value == 0 or button_pressed.is_set():
+            continue
+        _log.debug("button pressed")
+        button_pressed.set()
+
+
+def serve_camera_only(
+    loop: asyncio.AbstractEventLoop,
+    camera: Picamera2,
+    image_tx: Channel[bytes],
+    button_pressed: Event,
+    terminate: Event,
+    delay: float = 0.1,
+) -> None:
+    while not terminate.is_set():
+        time.sleep(delay)
+        if not button_pressed.is_set():
+            continue
+        _log.debug("capture image")
+        image = camera.capture_image()
+        assert isinstance(image, Image)
+        with io.BytesIO() as b:
+            image.save(b, format="jpeg")
+            buf = b.getvalue()
+        _log.debug("send image")
+        asyncio.run_coroutine_threadsafe(image_tx.put(buf), loop)
+        button_pressed.clear()
 
 
 def serve_camera(
@@ -78,7 +119,8 @@ async def client(delay: float = 0.1) -> None:
     post_url = os.environ.get("POST_IMAGE_URL", "localhost:1323")
     button_pin = os.environ.get("BUTTON", "GPIO26")
     loop = asyncio.get_event_loop()
-    ch: Channel[bytes] = Channel(10)
+    image_ch: Channel[bytes] = Channel(10)
+    button_pressed = Event()
     terminate = Event()
 
     def quit() -> None:
@@ -93,9 +135,12 @@ async def client(delay: float = 0.1) -> None:
         acquire_camera() as camera,
         concurrent.futures.ThreadPoolExecutor() as pool,
     ):
-        serve = loop.run_in_executor(pool, serve_camera, loop, button, camera, ch, terminate, delay)
-        receive = asyncio.create_task(receive_camera(raspi_secret, post_url, ch))
-        await asyncio.wait([serve, receive], return_when=asyncio.FIRST_COMPLETED)
+        sbutton = loop.run_in_executor(pool, serve_button, button, button_pressed, terminate, delay)
+        scamera = loop.run_in_executor(
+            pool, serve_camera_only, loop, camera, image_ch, button_pressed, terminate, delay
+        )
+        receive = asyncio.create_task(receive_camera(raspi_secret, post_url, image_ch))
+        await asyncio.wait([sbutton, scamera, receive], return_when=asyncio.FIRST_COMPLETED)
 
 
 def run_client() -> None:
